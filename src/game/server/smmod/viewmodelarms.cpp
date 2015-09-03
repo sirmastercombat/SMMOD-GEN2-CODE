0eb7d86469d4e8e7720f6da51cbb4f30f41f64d8
sirmastercombat <combat.master@yahoo.com>
2015-09-02 22:35:42 -0500

    Added Cremator

---
+++ src/game/server/smmod/viewmodelarms.cpp
@@ -0,0 +1,59 @@
+#include "cbase.h"
+#include "hl2_player.h"
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+#ifdef SMMOD
+
+class CViewModelArms : public CBaseAnimating
+{
+	DECLARE_CLASS(CViewModelArms, CBaseAnimating);
+	DECLARE_SERVERCLASS();
+public:
+};
+LINK_ENTITY_TO_CLASS(viewmodelarms, CViewModelArms);
+IMPLEMENT_SERVERCLASS_ST(CViewModelArms, DT_ViewModelArms)
+END_SEND_TABLE()
+
+//Commented out until further notice
+/*
+class CViewModelLeg : public CBaseAnimating
+{
+	DECLARE_CLASS(CViewModelLeg, CBaseAnimating);
+	DECLARE_SERVERCLASS();
+public:
+	virtual void HandleAnimEvent(animevent_t *pEvent);
+};
+void CViewModelLeg::HandleAnimEvent(animevent_t *pEvent)
+{
+	switch(pEvent->event)
+	{
+	case EVENT_WEAPON_MELEE_HIT:
+		Msg("FOOT ACTIVATED!");
+		return;
+	}
+	//Pass it through as normal for now...
+	BaseClass::HandleAnimEvent(pEvent);
+}
+LINK_ENTITY_TO_CLASS(viewmodelleg, CViewModelLeg);
+IMPLEMENT_SERVERCLASS_ST(CViewModelLeg, DT_ViewModelLeg)
+END_SEND_TABLE()
+*/
+void ChangeArms_f(const CCommand &args)
+{
+	CHL2_Player *local_player = (CHL2_Player*) UTIL_GetCommandClient();
+	
+	if (engine->PrecacheModel(args.Arg(1), true) == 1 || engine->IsModelPrecached(args.Arg(1)))
+	{
+		local_player->m_ViewModelArm->SetModel(args.Arg(1));;//Set the current model now
+	}
+	else
+	{
+		Warning("Error: Unable to load model!");
+	}
+
+}
+
+ConCommand ChangeArms("changearms", ChangeArms_f);
+
+#endif
